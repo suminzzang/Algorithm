2f30e774ecdcf8baf2048a8d725d8fc0839fd064
suminzzang <hchyi259@naver.com>
2024-11-05 04:49:34 +0900

    [Update] Binary Search

--- Theory/BinarySearch/BinarySearch.cpp
+++ Theory/BinarySearch/BinarySearch.cpp
@@ -2,19 +2,56 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(int num, int value)
+{
+    return num > value;
+}
+
+void binarySearch(vector<int> input, int numToFind)
+{
+    auto it = lower_bound(input.begin(), input.end(), numToFind);
+    cout << "using Library " << it-input.begin() << endl;
+
+    int start = 0;
+    int end = input.size() - 1; // 끝 인덱스는 size() - 1
+
+    while (start <= end) // <=로 변경
+    {
+        int mid = (start + end) / 2;
+        if (input[mid] == numToFind)
+        {
+            cout << "Not Using Library " << mid << endl;
+            break;
+        }
+        else if (input[mid] > numToFind)
+        {
+            end = mid - 1; // end를 mid - 1로 이동
+        }
+        else
+        {
+            start = mid + 1; // start를 mid + 1로 이동
+        }
+    }
+
+
+    reverse(input.begin(), input.end());
+    it = lower_bound(input.begin(), input.end(), numToFind,compare);
+    it = lower_bound(input.begin(), input.end(), numToFind, greater<int>());
+
+    cout << "using Library " << it - input.begin() << endl;
+
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    vector<int> input = { 1,2,4,7,8,11,15,20 };
+    binarySearch(input, 8);
+
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
