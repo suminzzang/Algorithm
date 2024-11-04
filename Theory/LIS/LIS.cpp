640bb3ff64c340f4119998808a5d1180040142e3
suminzzang <hchyi259@naver.com>
2024-11-05 04:21:13 +0900

    [Update] Binary Search

--- Theory/LIS/LIS.cpp
+++ Theory/LIS/LIS.cpp
@@ -2,10 +2,62 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
+using namespace std;
+
+
+void LISDP(vector<int> input)
+{
+    vector<int> LIS(input.size(), 1);
+
+    for (int a = 0; a < input.size(); a++)
+    {
+        for (int b = 0; b < a; b++)
+        {
+            if (input[a] > input[b])
+            {
+                LIS[a] = max(LIS[a], LIS[b] + 1);
+            }
+        }
+    }
+
+    for (auto& a : LIS)
+    {
+        cout << a << " ";
+    }
+    cout << endl;
+
+}
+
+void LISBinarySearch(vector<int> input)
+{
+    vector<int> LIS;
+
+    for (auto& a : input)
+    {
+        auto it = lower_bound(LIS.begin(), LIS.end(), a);
+
+        if (it == LIS.end())
+        {
+            LIS.push_back(a);
+        }
+        else
+        {
+            LIS[it - LIS.begin()] = a;
+            //*it = a;
+        }
+    }
+
+    cout << LIS.size() << endl;
+}
 int main()
 {
     std::cout << "Hello World!\n";
+    vector<int> input = { 1,5,4,2,3,8,6,7,8,3,4,5 };
+    LISDP(input);
+    LISBinarySearch(input);
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
